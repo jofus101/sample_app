5ba576ba86c3220a80b7f711a9178b58b7f4210c
Joseph Rosenberg <jofus101@gmail.com>
2013-06-20 20:20:14 -0700

    Revert "-a"
    This reverts commit 78c15b03c2330d8732e48b2bbe8a3355e85965ac.
    
    wanted to add a comment to the commit

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -18,18 +18,16 @@ describe User do
   end
 
   subject { @user }
-  describe "should have all the spec'ed attributes" do
-		it { should respond_to(:name) }
-		it { should respond_to(:email) }
-		it { should respond_to(:password_digest) }
-		it { should respond_to(:password) }
-		it { should respond_to(:password_confirmation) }
-		it { should respond_to(:remember_token) }
-		it { should respond_to(:authenticate) }
-		
-		it { should be_valid }
-	end
-		
+
+  it { should respond_to(:name) }
+  it { should respond_to(:email) }
+  it { should respond_to(:password_digest) }
+  it { should respond_to(:password) }
+  it { should respond_to(:password_confirmation) }
+  it { should respond_to(:authenticate) }
+  
+  it { should be_valid }
+  
   describe "when password is not present" do
     before { @user.password = @user.password_confirmation = " " }
     it { should_not be_valid }
