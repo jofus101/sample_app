99220d8a0650359e699a88901062d17a66105f6a
Joseph Rosenberg <jofus101@gmail.com>
2013-06-20 23:25:18 -0700

    Add update user functionality and make failing that pass tests

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -4,23 +4,21 @@ describe "UserPages" do
 
   subject { page }
 
-  describe "signup page" do
-    before { visit signup_path }
-      
-    it { should have_selector('h1',    text: 'Sign up') }
-    it { should have_selector('title', text: full_title('Sign up')) }
-  end
-    
   describe "profile page" do
-	let(:user) { FactoryGirl.create(:user) }
-	before { visit user_path(user) }
-	
-	it { should have_selector('h1',    text: user.name) }
-	it { should have_selector('title', text: user.name) }
+  	let(:user) { FactoryGirl.create(:user) }
+  	before { visit user_path(user) }
+  	
+  	it { should have_selector('h1',    text: user.name) }
+  	it { should have_selector('title', text: user.name) }
   end
   
   describe "signup" do
     before { visit signup_path }
+
+    describe "page" do
+      it { should have_selector('h1',    text: 'Sign up') }
+      it { should have_selector('title', text: full_title('Sign up')) }  
+    end
 
     let(:submit) { "Create my account" }
 
@@ -54,5 +52,22 @@ describe "UserPages" do
       end
     end
   end
+
+describe "edit" do
+  let(:user) { FactoryGirl.create(:user) }
+  before { visit edit_user_path(user) }
+
+  describe "page" do
+    it { should have_selector('h1',    text: "Update your profile") }
+    it { should have_selector('title', text: "Edit user") }
+    it { should have_link('change', href: 'http://gravatar.com/emails') }
+  end
+
+  describe "with invalid information" do
+    before { click_button "Save changes" }
+
+    it { should have_content('error') }
+  end
+end
 
 end
