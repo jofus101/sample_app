1ac240714614796d531073264b476f8b1eadd270
Joseph Rosenberg <jofus101@gmail.com>
2013-06-23 15:03:22 -0700

    Add authorization to edit and update of user.  Not user specific though, so anyone can add and update atm.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,12 @@
 class UsersController < ApplicationController
+  before_filter :signed_in_user, only: [:edit, :update]
   
   def show
     @user = User.find(params[:id])
   end
   
   def new
-	@user = User.new
+    @user = User.new
   end
   
   def create
@@ -23,15 +24,20 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
-def update
-  @user = User.find(params[:id])
-  if @user.update_attributes(params[:user])
-    flash[:success] = "Profile updated"
-    sign_in @user
-    redirect_to @user
-  else
-    render 'edit'
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(params[:user])
+      flash[:success] = "Profile updated"
+      sign_in @user
+      redirect_to @user
+    else
+      render 'edit'
+    end
   end
-end
 
+private
+  def signed_in_user
+    redirect_to signin_url, notice: "Please sign in." unless signed_in?
+  end
+  
 end
