3ae8915402a236b8dfe2cb4013d885cf4eb2b7c8
Joseph Rosenberg <jofus101@gmail.com>
2013-06-23 20:25:21 -0700

    Add Test to ensure links don't show up when not signed in.

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -19,31 +19,35 @@ describe "Authentication" do
 
       it { should have_selector('title', text: 'Sign in') }
       it { should have_error_message('Invalid') }
-			
-			describe "after visiting another page" do
+      
+      describe "after visiting another page" do
         before { click_link "Home" }
         it { should_not have_error_message }
       end
     end		
 		
-		describe "with valid information" do
-			let(:user) { FactoryGirl.create(:user) }
-			before { sign_in(user) }
+    describe "with valid information" do
+      let(:user) { FactoryGirl.create(:user) }
+      before { sign_in(user) }
 
-			it { should have_selector('title', text: user.name) }
+      it { should have_selector('title', text: user.name) }
 
       it { should have_link('Users', href: users_path) }
-			it { should have_link('Profile', href: user_path(user)) }
+      it { should have_link('Profile', href: user_path(user)) }
       it { should have_link('Settings', href: edit_user_path(user)) }
-			it { should have_link('Sign out', href: signout_path) }
+      it { should have_link('Sign out', href: signout_path) }
 
-			it { should_not have_link('Sign in', href: signin_path) }
-			
-			describe "followed by signout" do
-				before { click_link "Sign out" }
-				it { should have_link('Sign in') }
-			end
-		end
+      it { should_not have_link('Sign in', href: signin_path) }
+
+      describe "followed by signout" do
+        before { click_link "Sign out" }
+        it { should have_link('Sign in') }
+        it { should_not have_link('Users', href: users_path) }
+        it { should_not have_link('Profile', href: user_path(user)) }
+        it { should_not have_link('Settings', href: edit_user_path(user)) }
+        it { should_not have_link('Sign out', href: signout_path) }
+      end
+    end
   end
 
   describe "authorization" do
