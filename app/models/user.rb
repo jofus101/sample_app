a81432e06ef091412d084f16f6a248e9a34f8621
Joseph Rosenberg <jofus101@gmail.com>
2013-09-19 18:34:27 -0700

    Changed it to be in line with Rails 3.xx not 4.0

--- app/models/user.rb
+++ app/models/user.rb
@@ -18,10 +18,10 @@ class User < ActiveRecord::Base
   has_many :microposts, dependent: :destroy
   has_many :relationships, foreign_key: "follower_id", dependent: :destroy
   has_many :reverse_relationships, foreign_key: "followed_id", 
-                                    class_name: "Relationship",
-                                    dependent: :destroy
+                                   class_name: "Relationship",
+                                   dependent: :destroy
   has_many :followed_users, through: :relationships, source: :followed
-  has_many :followers, through: :reverse_relationships, #source: :follower
+  has_many :followers, through: :reverse_relationships, source: :follower
   
 	before_save { email.downcase! }
   before_save :create_remember_token
@@ -41,7 +41,7 @@ class User < ActiveRecord::Base
   end
 	
   def following?(other_user)
-    relationships.find_by(followed_id: other_user.id)
+    relationships.find_by_followed_id( other_user.id)
   end
 
   def follow!(other_user)
@@ -49,7 +49,7 @@ class User < ActiveRecord::Base
   end
 
   def unfollow!(other_user)
-    relationships.find_by(followed_id: other_user.id).destroy!
+    relationships.find_by_followed_id(other_user.id).destroy
   end
 
 	private 
