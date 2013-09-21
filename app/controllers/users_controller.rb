45ab0ad549537e0f1e2d85b3ebe10c673adb89cb
Joseph Rosenberg <jofus101@gmail.com>
2013-09-20 17:07:55 -0700

    Create a follower/following page with mini icons 11.2.3

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_filter :signed_in_user, only: [:edit, :update, :index, :destroy]
+  before_filter :signed_in_user, only: [:edit, :update, :index, :destroy, :following, :followers]
   before_filter :signed_out_user, only: [:new, :create]
   before_filter :correct_user, only: [:edit, :update]
   before_filter :admin_user, only: :destroy
@@ -52,20 +52,34 @@ class UsersController < ApplicationController
     redirect_to users_url
   end
 
-private
-  def signed_out_user
-    if signed_in?
-      redirect_to root_path, notice: "You've already signed up! You're signed in right now.  :)"
-    end
-  end
-
-  def correct_user
+  def following
+    @title = "Following"
     @user = User.find(params[:id])
-    redirect_to(root_path) unless current_user?(@user)
+    @users = @user.followed_users.paginate(page: params[:page])
+    render 'show_follow'
   end
 
-  def admin_user
-      redirect_to(root_path) unless current_user.admin?
+  def followers
+    @title = "Followers"
+    @user = User.find(params[:id])
+    @users = @user.followers.paginate(page: params[:page])
+    render 'show_follow'
   end
+
+  private
+    def signed_out_user
+      if signed_in?
+        redirect_to root_path, notice: "You've already signed up! You're signed in right now.  :)"
+      end
+    end
+
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_path) unless current_user?(@user)
+    end
+
+    def admin_user
+        redirect_to(root_path) unless current_user.admin?
+    end
 
 end
